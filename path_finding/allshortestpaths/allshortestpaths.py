985007b438058391c57a668f50f1d18dffd0fbf0
Gustavo Pereira <gustavo.3.5.p@gmail.com>
2024-03-18 21:59:20 -0300

    spark on collab

--- path_finding/allshortestpaths/allshortestpaths.py
+++ path_finding/allshortestpaths/allshortestpaths.py
@@ -1,6 +1,11 @@
 # // tag::imports[]
 from graphframes import *
 # // end::imports[]
+from pyspark.context import SparkContext
+from pyspark.sql.session import SparkSession
+sc = SparkContext.getOrCreate()
+#spark = SparkSession.builder.getOrCreate()
+spark = SparkSession.builder.master("local[*]").config("spark.jars.packages", "graphframes:graphframes:0.8.3-spark3.5-s_2.12").getOrCreate()
 
 # // tag::load-graph-frame[]
 v = spark.read.csv("data/transport-nodes.csv", header=True)
